d761cc34b4933f6ecc710519e47461282ce133f9
shannonpearson <shannon.pearson@doctolib.com>
2020-04-19 19:31:04 -0400

    creates articles resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  get 'welcome/index' # map requests to /welcome/index to welcome controller's 'index' action
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'welcome#index' # map requests to app route to the welcome controller's 'index' action
+
+  resources :articles
+  
+  root 'welcome#index' # map requests to *app route* to the welcome controller's 'index' action
 end
