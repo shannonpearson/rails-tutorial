51f1aed77894d4a741d3bbbb9081e1162a4209cc
shannonpearson <shannon.pearson@doctolib.com>
2020-04-20 10:44:44 -0400

    add edit view and functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,17 +1,21 @@
 class ArticlesController < ApplicationController
-  def index 
+  def index # on /articles GET route page load
     @articles = Article.all
   end
 
-  def show
+  def show # on /articles/:id GET route page load
     @article = Article.find(params[:id])
   end
 
-  def new
+  def new # on /articles/new GET route page load
     @article = Article.new
   end 
 
-  def create
+  def edit # on /articles/:id/edit route page load
+    @article = Article.find(params[:id])
+  end
+
+  def create # on /articles POST route call from /articles/new page
     @article = Article.new(article_params) # initialize Article model - defined in models/article.rb
     if @article.save
       redirect_to @article
@@ -20,8 +24,19 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def update # on /articles/:id PUT/PATCH(?) route call from /articles/:id/edit page
+    # accepts hash containing attributes to update
+    @article = Article.find(params[:id])
+   
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
   private
   def article_params
-    params.require(:article).permit(:title, :text) #specify allowed parameters ('strong parameters') for security
+    params.require(:article).permit(:title, :text) # specify allowed parameters ('strong parameters') for security on create or update
   end
 end
