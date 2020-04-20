51a922b767d044c109662f7386b2368e24e63dd6
shannonpearson <shannon.pearson@doctolib.com>
2020-04-19 21:40:14 -0400

    add article form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,9 @@
 class ArticlesController < ApplicationController
-  def new
+  def new # nothing happens if this is removed so what is it doing?
+  end 
+
+  def create
+    render plain: params[:article].inspect # simple hash with key of plain
+    # this displays: <ActionController::Parameters {"title"=>"test", "text"=>"hi"} permitted: false>
   end
 end
